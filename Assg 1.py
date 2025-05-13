a4a8f34e6f04c2b57fff378c2ea6fed5873f4569
holysanity <59819942+holysanity@users.noreply.github.com>
2025-05-13 16:35:15 +0530

    Add files via upload

---
+++ Assg 1.py
@@ -0,0 +1,26 @@
+#1)
+'''
+a = float(input('Enter the first number: '))
+b = float(input('Enter the second number: '))
+
+Addition = a+b
+Subtraction = a-b
+Multiplication = a*b
+Division = a/b
+
+print('Addition: ', Addition,
+'\nSubtraction:', Subtraction,
+'\nMultiplication:', Multiplication,
+'\nDivision: ', Division)
+'''
+
+#2)
+'''
+c = input('Enter your first name: ')
+d = input('Enter your last name: ')
+e = c + ' ' +d
+
+print('Hello, '+ e +'! Welcome to the Python Program.')
+'''
+
+#3)
